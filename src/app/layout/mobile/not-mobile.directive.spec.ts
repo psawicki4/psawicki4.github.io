b4aad4f955db2a9143e5de122c96899cf1d536f8
psawicki <psawicki4@gmail.com>
2022-07-15 22:04:10 +0200

    added husky and cypress

--- src/app/layout/mobile/not-mobile.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NotMobileDirective } from './not-mobile.directive';
-
-describe('NotMobileDirective', () => {
-  /*it('should create an instance', () => {
-    const directive = new NotMobileDirective();
-    expect(directive).toBeTruthy();
-  });*/
-});
