ac84a8e1bd9ef51dc15996e6e1d11ed887f4e070
psawicki <psawicki4@gmail.com>
2022-08-29 18:36:00 +0200

    cypress init

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,32 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {ColorsModule} from "./components/colors/colors.module";
+import {LanguageModule} from "./components/language/language.module";
+import {CardModule} from "./components/card/card.module";
+import {TranslateLoader, TranslateModule} from "@ngx-translate/core";
+import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpLoaderFactory} from "./app.module";
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatToolbarModule,
+        ColorsModule,
+        LanguageModule,
+        CardModule,
+        TranslateModule.forRoot({
+          defaultLanguage: 'pl',
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        }),
+        HttpClientModule
       ],
       declarations: [
         AppComponent
