637f728d48c950de9f547e979d4d19da16afd4fb
psawicki <psawicki4@gmail.com>
2024-06-29 16:54:17 +0200

    Auto-generated commit

--- chunk-NB42QVCY.js
+++
@@ -1,1 +0,0 @@
-import{Gb as i,Hb as o,bc as r,ic as s,ka as a}from"./chunk-H6AV5CDL.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=a({type:e,selectors:[["psa-animations"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,m){n&1&&(i(0,"p"),r(1,"animations works!"),o())},changeDetection:0});let t=e;return t})();export{d as AnimationsComponent};
---
+++ chunk-V67VVLP7.js
@@ -0,0 +1,1 @@
+import{Kb as i,Lb as o,fc as r,ma as a,mc as s}from"./chunk-HJF5PFZU.js";var d=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=a({type:e,selectors:[["psa-animations"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,m){n&1&&(i(0,"p"),r(1,"animations works!"),o())},changeDetection:0});let t=e;return t})();export{d as AnimationsComponent};
