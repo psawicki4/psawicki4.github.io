fbac5f591728774ecad3413b7077d8e524f9fdf2
psawicki <psawicki4@gmail.com>
2024-11-05 22:03:27 +0100

    home font fix

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,10 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { MatAnchor } from "@angular/material/button";
 import { MatList, MatListItem } from "@angular/material/list";
 import { TranslatePipe } from "@ngx-translate/core";
 import { CardComponent } from "../../components/card/card.component";
+import { NgClass } from '@angular/common';
+import { IsMobileService } from '../../services/is-mobile.service';
 
 @Component({
   selector: 'psa-home',
@@ -12,7 +14,8 @@ import { CardComponent } from "../../components/card/card.component";
     MatList,
     MatListItem,
     MatAnchor,
-    TranslatePipe
+    TranslatePipe,
+    NgClass
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
@@ -20,4 +23,9 @@ import { CardComponent } from "../../components/card/card.component";
 })
 export class HomeComponent {
 
+  isMobileService = inject(IsMobileService);
+
+  get isMobile() {
+    return this.isMobileService.isMobile();
+  }
 }
