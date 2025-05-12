9c37ba1741d172dce6ec4d55642a9571fc27efc2
psawicki <psawicki4@gmail.com>
2025-05-12 15:27:10 +0200

    Auto-generated commit

---
+++ chunk-HGEFC6MC.js
@@ -0,0 +1,1 @@
+import{Ob as o,Pb as i,hc as a,tb as n}from"./chunk-6CQJFHGG.js";var p=(()=>{class e{static{this.\u0275fac=function(t){return new(t||e)}}static{this.\u0275cmp=n({type:e,selectors:[["psa-animations"]],decls:2,vars:0,template:function(t,r){t&1&&(o(0,"p"),a(1,"Work in progress... :)"),i())},encapsulation:2,changeDetection:0})}}return e})();export{p as AnimationsComponent};
