05d88e8ad70ba34c1e1497530d8265e929d65b40
psawicki <psawicki4@gmail.com>
2025-08-04 21:45:20 +0200

    feat: enhance 3D model viewer with new models, adjustable lighting, and improved layout

--- src/app/pages/model-3d/model-3d.component.ts
+++ src/app/pages/model-3d/model-3d.component.ts
@@ -1,17 +1,22 @@
-import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, viewChild, effect, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, viewChild, effect, inject, signal } from '@angular/core';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { CardComponent } from "../../components/card/card.component";
-import { TranslatePipe } from '@ngx-translate/core';
+import { TranslatePipe, TranslateService } from '@ngx-translate/core';
 import { MatButton } from '@angular/material/button';
+import { MatSlideToggle, MatSlideToggleChange } from '@angular/material/slide-toggle';
+import { MatSliderModule } from "@angular/material/slider";
+import { V } from '@angular/cdk/keycodes';
 
 @Component({
   selector: 'psa-model-3d',
   imports: [
     CardComponent,
     TranslatePipe,
-    MatButton
+    MatButton,
+    MatSlideToggle,
+    MatSliderModule
   ],
   templateUrl: './model-3d.component.html',
   styleUrl: './model-3d.component.scss',
@@ -19,13 +24,12 @@ import { MatButton } from '@angular/material/button';
 })
 export class Model3DComponent implements OnDestroy {
 
+  translate = inject(TranslateService);
   canvas = viewChild<ElementRef>('canvas');
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(75, 1, 0.1, 1000);
-  ambientLight = new THREE.AmbientLight(0xffffff, 0.6); // Światło otoczenia - zwiększone
-  directionalLight = new THREE.DirectionalLight(0xffffff, 1.2); // Światło kierunkowe - zwiększone
-  pointLight = new THREE.PointLight(0xffffff, 0.8, 10); // Dodatkowe światło punktowe
-  hemisphereLight = new THREE.HemisphereLight(0xffffff, 0x444444, 0.4); // Światło półkuliste
+  camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000);
+  ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
+  directionalLight = new THREE.DirectionalLight(0xffffff, 5);
   threeRenderer = new THREE.WebGLRenderer({
     antialias: true,
     alpha: true
@@ -35,6 +39,8 @@ export class Model3DComponent implements OnDestroy {
   mixer?: THREE.AnimationMixer; // Mixer do zarządzania animacjami
   clock = new THREE.Clock(); // Zegar do śledzenia czasu
   loader = new GLTFLoader();
+  rotationSpeed = 0.01;
+  credits: string = '';
 
   constructor() {
     effect(() => {
@@ -60,33 +66,38 @@ export class Model3DComponent implements OnDestroy {
     // Dodaj oświetlenie do sceny
     this.scene.add(this.ambientLight);
     this.scene.add(this.directionalLight);
-    this.scene.add(this.pointLight);
-    this.scene.add(this.hemisphereLight);
-
     // Ustaw pozycję światła kierunkowego
     this.directionalLight.position.set(5, 5, 5);
-    this.directionalLight.castShadow = true;
 
-    // Ustaw pozycję światła punktowego
-    this.pointLight.position.set(-3, 3, 3);
-
-    // Konfiguracja światła półkulistego
-    this.hemisphereLight.position.set(0, 10, 0);
-
-    // Włącz cienie w rendererze
-    this.threeRenderer.shadowMap.enabled = true;
-    this.threeRenderer.shadowMap.type = THREE.PCFSoftShadowMap;
     this.loadModel();
     this.camera.position.z = 5;
     this.threeRenderer.setAnimationLoop(() => this.animate());
   }
 
-  loadModel(name: string = 'bird_orange') {
+  loadModel(name: string = 'butterfly') {
     if (this.model) {
       this.scene.remove(this.model);
     }
+    let scalar = 1;
+    switch (name) {
+      case 'butterfly':
+        this.credits = this.translate.instant('MODEL_3D.butterfly__credits');
+        scalar = 0.02;
+        break;
+      case 'stylized_ww1_plane':
+        this.credits = this.translate.instant('MODEL_3D.plane__credits');
+        scalar = 2;
+        break;
+      case "sea_turtle":
+        this.credits = this.translate.instant('MODEL_3D.turtle__credits');
+        break;
+      default:
+        this.credits = '';
+        scalar = 1;
+    }
     this.loader.load(`assets/models/${name}.glb`, (gltf) => {
       this.model = gltf.scene;
+      this.model.scale.setScalar(scalar);
 
       // Włącz cienie dla wszystkich obiektów w modelu
       this.model.traverse((child) => {
@@ -118,9 +129,17 @@ export class Model3DComponent implements OnDestroy {
       this.mixer.update(delta);
     }
     if (this.model) {
-      this.model.rotation.y += 0.01;
+      this.model.rotation.y += this.rotationSpeed;
     }
 
     this.threeRenderer.render(this.scene, this.camera);
+  }
+
+  toggleRotation(e: MatSlideToggleChange) {
+    this.rotationSpeed = e.checked ? 0.01 : 0;
+  }
+  changeLightIntensity(e: Event) {
+    const value = (e.target as HTMLInputElement).valueAsNumber;
+    this.directionalLight.intensity = value;
   }
 }
