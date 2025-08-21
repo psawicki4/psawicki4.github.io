e36f5d1cdf1d5fa60ff8c4028d64792cfcb76ceb
psawicki <psawicki4@gmail.com>
2025-08-21 22:01:42 +0200

    feat: update translations for model names and credits, refactor credits handling in component

--- src/app/pages/model-3d/model-3d.component.ts
+++ src/app/pages/model-3d/model-3d.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, viewChild, effect, inject } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, OnDestroy, viewChild, effect, inject, signal } from '@angular/core';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
@@ -25,6 +25,7 @@ export class Model3DComponent implements OnDestroy {
 
   translate = inject(TranslateService);
   canvas = viewChild<ElementRef>('canvas');
+
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(50, 1, 0.1, 1000);
   ambientLight = new THREE.AmbientLight(0xffffff, 0.8);
@@ -43,10 +44,11 @@ export class Model3DComponent implements OnDestroy {
   mixer?: THREE.AnimationMixer;
   clock = new THREE.Clock();
   loader = new GLTFLoader();
+
   loading = false;
   rotationSpeed = 0.01;
   modelAnimationEnabled = true;
-  credits: string = '';
+  credits = signal('');
 
   constructor() {
     effect(() => {
@@ -84,7 +86,7 @@ export class Model3DComponent implements OnDestroy {
     if (this.model) {
       this.scene.remove(this.model);
     }
-    this.credits = this.translate.instant(`MODEL_3D.${name}__credits`);
+    this.credits.set(this.translate.instant(`MODEL_3D.${name}__credits`));
     this.loader.load(`assets/models/${name}.glb`, (gltf) => {
       this.model = gltf.scene;
       
