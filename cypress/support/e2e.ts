0eead36a69a1b8d804eeb0a4a2f795036bb55846
psawicki <psawicki4@gmail.com>
2025-10-25 21:32:20 +0200

    form e2e tests

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -16,5 +16,8 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+// Import form helpers
+import './form-helpers'
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
