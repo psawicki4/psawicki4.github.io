60534c4b37c648e7adb5264fef3c8e7cfa861a2f
psawicki <psawicki4@gmail.com>
2025-12-10 16:53:04 +0100

    ssr test

--- src/server.ts
+++ src/server.ts
@@ -51,16 +51,16 @@ app.use((req, res, next) => {
  * Start the server if this module is the main entry point, or it is ran via PM2.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
-if (isMainModule(import.meta.url) || process.env['pm_id']) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, (error) => {
-    if (error) {
-      throw error;
-    }
+// if (isMainModule(import.meta.url) || process.env['pm_id']) {
+//   const port = process.env['PORT'] || 4000;
+//   app.listen(port, (error) => {
+//     if (error) {
+//       throw error;
+//     }
 
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
+//     console.log(`Node Express server listening on http://localhost:${port}`);
+//   });
+// }
 
 /**
  * Request handler used by the Angular CLI (for dev-server and during build) or Firebase Cloud Functions.
