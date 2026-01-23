95a12b4e77b5b0662c0aade66d72581842eecf42
psawicki <psawicki4@gmail.com>
2026-01-23 15:28:56 +0100

    fix vercel function

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 export default async (req, res) => {
-  const { reqHandler } = await import('../dist/personal-website/server/server.mjs');
+  const { reqHandler } = await import('../dist/apps/personal-website/server/server.mjs');
   return reqHandler(req, res);
 };
